490e30256062cf81b989482a62f45d713ee15a41
r-yin <98623255+r-yin@users.noreply.github.com>
2023-03-21 00:30:18 -0400

    add comments

--- openmpi/helloworld.c
+++ openmpi/helloworld.c
@@ -1,14 +1,14 @@
-#include <mpi.h>
+#include <mpi.h> // include library
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
     int num_procs, rank;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Init(&argc, &argv); // initilization
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs); // commnicator, number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // communicator, process ID
 
-    printf("Hello world %d of %d\n", rank, num_procs);
+    printf("Hello world %d of %d\n", rank, num_procs); // the process that we want to run
 
     MPI_Finalize();
     return 0;
